7d9485d3279740338837a5f7f56278232a777d4d
Diogo V. Araújo <diogoviannaaraujo@gmail.com>
2021-08-18 19:41:55 -0300

    Node js bin file.

---
+++ bin/main.js
@@ -0,0 +1,13 @@
+// import {render} from 'ink'
+// import fs from 'fs'
+
+const fs = require('fs')
+const project = fs.readdirSync("./")
+console.log(project)
+
+//const projectPath = 
+//const moilerPath = 
+
+// generate temp manifest.json from manifest_base.json
+// run mcconfig
+
