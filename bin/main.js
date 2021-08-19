d83f4eb16800ac44bde45eb08f825e4bb0ebdd7c
Diogo V. Araújo <diogoviannaaraujo@gmail.com>
2021-08-18 23:30:11 -0300

    Deep update

--- bin/main.js
+++ bin/main.js
@@ -1,13 +1,37 @@
+#!/usr/bin/env node
+
 // import {render} from 'ink'
 // import fs from 'fs'
 
 const fs = require('fs')
-const project = fs.readdirSync("./")
-console.log(project)
+const path = require('path')
+const glob = require('glob')
+const execa = require('execa')
 
-//const projectPath = 
-//const moilerPath = 
+const projectPath = "."
+const moilerPath = path.dirname(__dirname)
+const idfPath = moilerPath + '/deps/esp-idf'
+const moddablePath = moilerPath + '/deps/moddable'
+
+const manifestBase = require(moilerPath + '/manifest_base.json')
 
 // generate temp manifest.json from manifest_base.json
-// run mcconfig
+const projectFiles = glob.sync(`{${projectPath}/,!(node_modules)/**/}*.js`)
+const projectFilesModules = projectFiles.reduce((acc, cur, i) => {
+    const s = cur.replace(".js", '')
+    acc[s] = '../.' + s
+    return acc
+}, {})
 
+const manifest = {
+    ...manifestBase,
+    modules: {
+        ...manifestBase.modules,
+        ...projectFilesModules
+    }
+}
+
+fs.writeFileSync(moilerPath + '/manifest.json', JSON.stringify(manifest));
+
+// run mcconfig
+execa(`source ${idfPath}/export.sh && cd ${moilerPath} && ${moilerPath}/deps/moddable/build/bin/mac/release/mcconfig -d -m -p esp32`)
